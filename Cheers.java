a40618bc5ef38f72ed0ffc59774b1b5ceff0807c
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-04 19:58:02 +0200

    cher1

--- Cheers.java
+++ Cheers.java
@@ -9,5 +9,28 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+
+            String a = args [0];
+            int num = Integer.parseInt(args[1]);
+            int b =  (int) a.length();
+            while ( b>1) {
+                 {
+                        char c = a.charAt(a.length()- b); 
+                System.out.println("Give ma an " + c + ": " + c + "!");
+                 b = b - 1 ;
+                }
+                
+                }
+                if (b==1){
+                        char c = a.charAt(a.length()- 1);
+                System.out.println("Give ma an " + c + ": " + c + "!");
+                }
+                
+            
+            System.out.println("What does it spell?");
+            while (num>0) {
+                System.out.println(a + "!!!");
+                num = num - 1;   
+            }
         }
 }
