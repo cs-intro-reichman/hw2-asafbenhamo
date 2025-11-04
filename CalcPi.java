ca297aa8676fadae7acb619375bd92dac7869bd5
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-04 22:07:42 +0200

    cal

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,21 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		double p = Math.PI;
+		double s = 0;
+		int a = 1;
+		int d = 3;
+		while (num>0) {
+		s = a - (1 / d);
+		d = d +2;
+		s = s +(1 / d);
+		d = d + 2 ;
+		num = num - 1;
+		}
+		 System.out.println("pi according to Java: " + p);
+		 System.out.println("pi, approximated:     " + s);
+
+
 	}
 }
