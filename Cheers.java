127da68cfd32f66a7928dc4178d8d39575452ef3
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-10 11:52:00 +0200

    c3

--- Cheers.java
+++ Cheers.java
@@ -6,6 +6,9 @@ Great work boaz!
 
 
 // Prints a crowd cheering output.
+
+import javax.print.DocFlavor.STRING;
+
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
@@ -13,6 +16,7 @@ public class Cheers {
             String a = args [0];
             int num = Integer.parseInt(args[1]);
             int b =  (int) a.length();
+            String u = "AEFHILMNORSX";
             if (b==1){
              
             }
@@ -20,8 +24,7 @@ public class Cheers {
                  {
                         char c = a.charAt(a.length()- b);
                         c = Character.toUpperCase(c);
-                        if ((c == 'A') || (c =='E') || (c =='F') || (c =='H') || (c =='I') || (c =='L') || (c =='M')
-                        || (c =='N') || (c =='O') || (c =='R') || (c =='S') || (c =='X')) {
+                        if ( u.indexOf(c) != -1) {
                          System.out.println("Give me an " + c + ": " + c + "!");
                         }
                         else System.out.println("Give me a  " + c + ": " + c + "!");
@@ -33,12 +36,12 @@ public class Cheers {
                 if (b==1){
                         char c = a.charAt(a.length()- b);
                         c = Character.toUpperCase(c);
-                        if ((c == 'A') || (c =='E') || (c =='F') || (c =='H') || (c =='I') || (c =='L') || (c =='M')
-                        || (c =='N') || (c =='O') || (c =='R') || (c =='S') || (c =='R')) {
+                        if ( u.indexOf(c) != -1) {
                          System.out.println("Give me an " + c + ": " + c + "!");
                         }
                         else System.out.println("Give me a  " + c + ": " + c + "!");
                         b = b - 1 ;
+                        
                 }
                 
             
